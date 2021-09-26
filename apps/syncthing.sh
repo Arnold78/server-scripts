1d2408ac8ae4da0b72ada7df0e34d74212287be6
arnold78 <arnoldadame78@gmail.com>
2021-09-26 12:28:05 -0500

    1

---
+++ apps/syncthing.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Install dependencies just in case
+sudo apt-get install apt-transport-https
+
+# Add the release PGP keys:
+sudo curl -s -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
+
+# Add the "stable" channel to your APT sources:
+echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+
+# Add the "candidate" channel to your APT sources:
+echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing candidate" | sudo tee /etc/apt/sources.list.d/syncthing.list
+
+# Increase preference of Syncthing's packages ("pinning")
+printf "Package: *\nPin: origin apt.syncthing.net\nPin-Priority: 990\n" | sudo tee /etc/apt/preferences.d/syncthing
+
+# Update and Install syncthing:
+sudo apt update
+sudo apt install syncthing
+
+
