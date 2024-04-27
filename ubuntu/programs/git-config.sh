043399b9e24ff75601d128d243172635a93371a0
Arnold78 <Arnoldadame78@gmail.com>
2024-04-26 23:47:06 -0500

    Update git-config.sh

--- ubuntu/programs/git-config.sh
+++ ubuntu/programs/git-config.sh
@@ -5,5 +5,5 @@ git config --global user.name  arnold78
 git config --global user.email arnoldadame78@gmail.com
 sudo git config --system core.editor nano
 
-# git config --global credential.helper cache
-# git config --global credential.helper 'cache --timeout=3600'
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=3600'
